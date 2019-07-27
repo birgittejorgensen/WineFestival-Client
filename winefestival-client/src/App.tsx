4b6f09ebc71a72be250d6a777407d9ad18b002b9
Bijo <bijo@itu.dk>
2019-07-27 19:35:30 +0200

    added side nav

--- winefestival-client/src/App.tsx
+++ winefestival-client/src/App.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import CheeseburgerMenu from 'cheeseburger-menu';
+import HamburgerMenu from 'react-hamburger-menu';
 
 const App: React.FC = () => {
   return (
-    <Container>
-      <Row className="row">
-        <Col xs={12}>
-          <h1>My New React Bootstrap SPA</h1>
-          <Button>Look, I'm a button!</Button>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <CheeseburgerMenu
+        isOpen={this.state.menuOpen}
+        closeCallback={this.closeMenu.bind(this)}
+      >
+        <MenuContent closeCallback={this.closeMenu.bind(this)} />
+      </CheeseburgerMenu>
+
+      <HamburgerMenu
+        isOpen={this.state.menuOpen}
+        menuClicked={this.openMenu.bind(this)}
+        width={32}
+        height={24}
+        strokeWidth={3}
+        rotate={0}
+        color="black"
+        borderRadius={0}
+        animationDuration={0.5}
+      />
+
+      <div style={contentStyles}>
+        <h1>Cheeseburger Menu demo</h1>
+        <p>Click the hamburger icon to open menu</p>
+      </div>
+    </div>
   );
 };
 
